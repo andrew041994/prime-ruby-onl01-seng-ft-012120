0a460cdc597d97985459d5efdc17dea15e9a060d
Alehandro Persaud <andrew041994@gmail.com>
2020-01-24 18:22:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime? (num)
     if num > 1
       return true if num % 1 && num
-    else num > 1
-      return false if num % 2 == 0
+    # else num > 1
+    #   return false if num % 2 == 0
 
     end
 end
