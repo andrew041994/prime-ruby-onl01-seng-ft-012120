fd5de71309c0828ca3cc58a047bc2a185fe72ca1
Alehandro Persaud <andrew041994@gmail.com>
2020-01-24 18:14:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
-def prime? (num)
-if num > 1
-  num % 1 && num
-  return true
-else
-  return false
 
+require 'prime'
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
 end
-
 
 end
