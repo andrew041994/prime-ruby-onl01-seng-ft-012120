feedaaec633e1355c6974822ac15781d69e30657
Alehandro Persaud <andrew041994@gmail.com>
2020-01-24 18:37:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,7 @@
+
+
+require 'prime'
+
 def prime? (num)
-    # if num > 1
-    #   return false if num % 2 == 0
-    # else
-    #   return true
-    # # else num > 1
-    # #   return false if num % 2 == 0
-    #
-    # end
-    # puts num
-    # def is_prime(num)
-  n = 2
-  while n < num
-    return false if num % n == 0
-    n += 1
-  end
-  true
-end
-# end
+  Prime.prime?(num)
+ end
